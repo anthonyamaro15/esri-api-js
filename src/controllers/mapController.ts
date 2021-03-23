0f94c2cffa665613b3b6a30d922984563a14e65c
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-03-23 08:26:36 -0700

    update(mapController): use esri-loader instead of @esri/core

--- src/controllers/mapController.ts
+++ src/controllers/mapController.ts
@@ -1,15 +1,17 @@
 import { RefObject } from "react";
-import Map from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
+import { loadModules } from 'esri-loader';
 
 class MapController {
-    #map?: Map;
-    #mapview?: MapView;
+    #map?: __esri.Map;
+    #mapview?: __esri.MapView;
+    #featureLayer?: __esri.FeatureLayer;
 
     initialMap = async (domRef: RefObject<HTMLDivElement>) => {
         if(!domRef.current) {
             return
         }
+
+        const [Map, MapView, FeatureLayer] = await loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer'])
 
         this.#map = new Map({
             basemap: 'gray-vector'
@@ -19,8 +21,18 @@ class MapController {
             container: domRef.current,
             map: this.#map,
             center: [-100.33, 25.69],
-            zoom: 5
-        })
+            zoom: 4
+        });
+
+        this.#featureLayer =  new FeatureLayer({
+            url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Chicago_Crime_Tracts/FeatureServer/0'
+        });
+
+        // add layer when mapview is loaded
+        this.#mapview?.when(() => {
+            if(!this.#featureLayer) return;
+            this.#map?.add(this.#featureLayer);
+        });
     }
 }
 
